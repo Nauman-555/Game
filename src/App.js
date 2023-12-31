bf8caa63e5d21aa6682547ab1f64cbc861574626
Nauman-555 <naumannzaib@gmail.com>
2023-12-31 21:52:49 +0500

    Responsive

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import About from './Components/About'
-
+import "./App.css";
 function App() {
   return (
     <div>
-      <h1>Rock,Paper & Scissor</h1>
+      <h1 className='tol'>Rock,Paper & Scissor</h1>
       
       <About/>
     </div>
   )
 }
-
 export default App
--- src/Components/About.js
+++ src/Components/About.js
@@ -1,110 +1,90 @@
-import React, { useState } from 'react'
-import '../App.css'
-import RockImage from './Images/Rock.jpeg'
-import PaperImage from './Images/Paper.jpg'
-import ScissorImage from './Images/Scissor.jpeg'
+import React, { useState } from "react";
+import "../App.css";
+import RockImage from "./Images/Rock.jpeg";
+import PaperImage from "./Images/Paper.jpg";
+import ScissorImage from "./Images/Scissor.jpeg";
 const About = () => {
-    const[top, setTop] = useState("Click on your choice!")
-    const[result, setResult] = useState('')
-    const[imageVariables, setImageVariables] = useState(true)
-    const handleRock = () =>{
+    const [top, setTop] = useState("  Click on your choice!");
+    const [result, setResult] = useState("");
+    const [ver, setVer] = useState("");
+    const [oop, setOop] = useState("");
+    const handleRock = () => {
         const num = Math.floor(Math.random() * 3);
-            if (num===0) {
-                setResult("Match Tied");
-            }
-            else if (num===1) {
-                setResult("You Loss");
-            }
-            else {
-                setResult("You Won")
-            }
-            setTop("");
-            setImageVariables(false);
-            setTimeout(() => {
-                setTop("     "+"    Click Again!")
-                setImageVariables(true);
-               }, 2000);
-            
-               setTimeout(() => {
-                setResult('');
-               }, 2000);
+        if (num === 0) {
+            setResult("Match Tied");
+            setOop("Opponent Choice: Rock");
+        } else if (num === 1) {
+            setResult("You Loss");
+            setOop("Opponent Choice: Paper");
+        } else {
+            setResult("You Won");
+            setOop("Opponent Choice: Scissor");
+        }
+        setTop("         Click Again!");
+
+        setVer("Your Choice: Rock");
     };
-    
-    const handlePaper = () =>{
+    const handlePaper = () => {
         const num = Math.floor(Math.random() * 3);
-            if (num===1) {
-                setResult("Match Tied");
-            }
-            else if(num===2) {
-                setResult("You Loss");
-            }
-            else {
-                setResult("You Won")
-            }
-            
-            setTop("");
-            setImageVariables(false);
-            setTimeout(() => {
-                setTop("     "+"    Click Again!")
-                setImageVariables(true);
-               }, 2000);
-            
-            // Clear the result after 2 second
-            setTimeout(() => {
-                setResult('');
-               }, 2000);
+        if (num === 1) {
+            setResult("Match Tied");
+            setOop("Opponent Choice: Paper");
+        } else if (num === 2) {
+            setResult("You Loss");
+            setOop("Opponent Choice: Scissor");
+        } else {
+            setResult("You Won");
+            setOop("Opponent Choice: Rock");
+        }
+
+        setTop("         Click Again!");
+
+        setVer("Your Choice: Paper");
     };
-    
-    const handleScissor = () =>{
+    const handleScissor = () => {
         const num = Math.floor(Math.random() * 3);
-            if (num===2) {
-                setResult("Match Tied");
-            }
-            else if(num===0) {
-                setResult("You Loss");
-            }
-            else{
-                setResult("You Won");
-            }
-            
-            setTop("");
-            setImageVariables(false);
-            setTimeout(() => {
-                setTop("     "+"    Click Again!")
-                setImageVariables(true);
-               }, 2000); 
+        if (num === 2) {
+            setResult("Match Tied");
+            setOop("Opponent Choice: Scissor");
+        } else if (num === 0) {
+            setResult("You Loss");
+            setOop("Opponent Choice: Rock");
+        } else {
+            setResult("You Won");
+            setOop("Opponent Choice: Paper");
+        }
 
-                 setTimeout(() => {
-                 setResult('');
-                }, 2000);
+        setTop("         Click Again!");
+
+        setVer("Your Choice: Scissor");
     };
-
-     // Use useEffect to set the top back after 2 seconds
- 
-    console.log('Top:', top);
-    let resultColor= "";
-    if(result=== 'You Won'){
-        resultColor = 'green';
+    let resultColor = "";
+    if (result === "You Won") {
+        resultColor = "green";
+    } else if (result === "You Loss") {
+        resultColor = "red";
     }
-    else if(result=== 'You Loss'){
-        resultColor = 'red';
+    if (result === "Match Tied") {
+        resultColor = "purple";
     }
-    if(result=== 'Match Tied'){
-        resultColor = 'purple';
-    }
-
-  return (
-    <div className='container'>
-
-<h2 style={{ whiteSpace: 'pre' }}>{top}</h2>  
-{  
-imageVariables && (<>
-      <img src={RockImage} alt="Rock"  onClick={handleRock}/>
-      <img src={PaperImage} alt="Paper" onClick={handlePaper}/>
-      <img src={ScissorImage} alt="Scissor" onClick={handleScissor}/>
-      </>)}      
-      <p style={{color: resultColor}}>{result}</p>
-    </div>
-  );
+    return (
+        <div className="res">
+            <h2 style={{ whiteSpace: "pre" }}>{top}</h2>
+            <div className="container">
+                <div className="ii">
+                <img src={RockImage} alt="Rock" onClick={handleRock} />
+                <img src={PaperImage} alt="Paper" onClick={handlePaper} />
+                <img src={ScissorImage} alt="Scissor" onClick={handleScissor} />
+                </div>
+                <div className="ip">
+                    <p className="one" style={{ color: resultColor }}>
+                        {result}
+                    </p>
+                    <p className="two">{ver}</p>
+                    <p>{oop}</p>
+                </div>
+            </div>
+        </div>
+    );
 };
-export default About
+export default About;
